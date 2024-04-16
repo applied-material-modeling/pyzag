29fbcce271978927ca075431c2b5de13fcb86dc2
messner <messner@anl.gov>
2024-04-16 11:10:01 -0500

    Added a more reasonable ODE example

--- test/test_ode.py
+++ test/test_ode.py
@@ -19,13 +19,86 @@ class LogisticODE(torch.nn.Module):
             (self.r - (2 * self.r * y) / self.K)[..., None],
         )
 
-    def exact(self, t, y0):
+    def exact(self, t):
+        y0 = self.y0(t.shape[-2])
         return (
             self.K
             * torch.exp(self.r * t)
             * y0[None, ...]
             / (self.K + (torch.exp(self.r * t) - 1) * y0[None, ...])
         )
+
+    def y0(self, nbatch):
+        return torch.linspace(0, 1, nbatch).reshape(nbatch, 1)
+
+
+class LinearSystem(torch.nn.Module):
+    """Linear system of equations"""
+
+    def __init__(self, n):
+        super().__init__()
+        self.n = n
+        Ap = torch.rand((n, n))
+        self.A = Ap.transpose(0, 1) * Ap
+
+        self.vals, self.vecs = torch.linalg.eigh(self.A)
+
+    def forward(self, t, y):
+        return torch.matmul(self.A.unsqueeze(0).unsqueeze(0), y.unsqueeze(-1)).squeeze(
+            -1
+        ), self.A.unsqueeze(0).unsqueeze(0).expand(
+            t.shape[0], t.shape[1], self.n, self.n
+        )
+
+    def exact(self, t):
+        c0 = torch.linalg.solve(
+            self.vecs.unsqueeze(0).expand(t.shape[1], -1, -1),
+            self.y0(t.shape[1]),
+        )
+        soln = torch.zeros((t.shape[:-1] + (self.n,)))
+
+        print(soln.shape)
+
+        for i in range(self.n):
+            soln += c0[:, i, None] * torch.exp(self.vals[i] * t) * self.vecs[:, i]
+
+        return soln
+
+    def y0(self, nbatch):
+        return torch.outer(torch.linspace(-1, 1, nbatch), torch.linspace(1, 2, self.n))
+
+
+class TestBackwardEulerTimeIntegrationLinear(unittest.TestCase):
+    def setUp(self):
+        self.n = 4
+        self.model = ode.BackwardEulerODE(LinearSystem(self.n))
+
+        self.nbatch = 5
+        self.ntime = 100
+
+        self.times = (
+            torch.linspace(0, 1, self.ntime)
+            .unsqueeze(-1)
+            .expand(-1, self.nbatch)
+            .unsqueeze(-1)
+        )
+
+    def test_integrate_forward(self):
+        nchunk = 8
+
+        for method in ["thomas", "pcr", "hybrid"]:
+            solver = nonlinear.RecursiveNonlinearEquationSolver(
+                self.model,
+                self.model.ode.y0(self.nbatch),
+                block_size=nchunk,
+                direct_solve_method=method,
+            )
+
+            nres = solver.solve(self.ntime, self.times)
+            eres = self.model.ode.exact(self.times)
+
+            self.assertEqual(nres.shape, eres.shape)
+            self.assertTrue(torch.allclose(nres, eres, rtol=1e-2))
 
 
 class TestBackwardEulerTimeIntegrationLogistic(unittest.TestCase):
@@ -42,8 +115,7 @@ class TestBackwardEulerTimeIntegrationLogistic(unittest.TestCase):
             .unsqueeze(-1)
         )
         self.y = torch.rand((self.ntime, self.nbatch, 1))
-
-        self.y0 = torch.linspace(0, 1, self.nbatch).reshape(self.nbatch, 1)
+        self.y0 = self.model.ode.y0(self.nbatch)
 
     def test_shapes(self):
         nchunk = 8
@@ -57,11 +129,14 @@ class TestBackwardEulerTimeIntegrationLogistic(unittest.TestCase):
 
     def test_integrate_forward(self):
         nchunk = 8
-        solver = nonlinear.RecursiveNonlinearEquationSolver(
-            self.model, self.y0, block_size=nchunk
-        )
-        nres = solver.solve(self.ntime, self.times)
-        eres = self.model.ode.exact(self.times, self.y0)
 
-        self.assertEqual(nres.shape, eres.shape)
-        self.assertTrue(torch.allclose(nres, eres, rtol=1e-3))
+        for method in ["thomas", "pcr", "hybrid"]:
+            solver = nonlinear.RecursiveNonlinearEquationSolver(
+                self.model, self.y0, block_size=nchunk, direct_solve_method=method
+            )
+
+            nres = solver.solve(self.ntime, self.times)
+            eres = self.model.ode.exact(self.times)
+
+            self.assertEqual(nres.shape, eres.shape)
+            self.assertTrue(torch.allclose(nres, eres, rtol=1e-3))
