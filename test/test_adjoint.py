12298463086c62449ae313768e5ec6c2dcb873f0
Mark Messner <mark.messner@gmail.com>
2024-06-20 16:55:39 -0500

    Ah, cleaning up

--- test/test_adjoint.py
+++ test/test_adjoint.py
@@ -158,7 +158,7 @@ class TestGeneralFunction(unittest.TestCase):
 
         self.ntime = 100
 
-        self.ref_time = torch.linspace(0, 0.05, self.ntime)
+        self.ref_time = torch.linspace(0, 0.01, self.ntime)
 
         self.f = 0.5
 
