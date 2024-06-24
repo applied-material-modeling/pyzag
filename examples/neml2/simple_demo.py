62f4381ca8e0d5c184917c7165e494f9b89900a5
Mark Messner <mark.messner@gmail.com>
2024-06-24 11:35:41 -0500

    Adjoint seems to work, issue with inplace for AD

--- examples/neml2/simple_demo.py
+++ examples/neml2/simple_demo.py
@@ -41,9 +41,7 @@ if __name__ == "__main__":
     )
 
     nmodel = neml2.load_model(fname, mname)
-    pmodel = model.NEML2Model(
-        nmodel, exclude_parameters=["elasticity.E", "elasticity.nu"]
-    )
+    pmodel = model.NEML2Model(nmodel)
 
     pmodel.yieldaabbsy.data = torch.tensor(100.0)
 
@@ -51,54 +49,20 @@ if __name__ == "__main__":
     initial_state = torch.zeros((nbatch, 7))
     forces = pmodel.collect_forces({"t": time, "E": strain})
 
-    F = forces[:10]
-    S = torch.rand_like(F)
-
-    print("BEFORE")
-    print(nmodel.named_parameters()["flow_rate.eta"].requires_grad)
-    print(pmodel.flow_rateaabbeta.requires_grad)
-
-    pmodel.flow_rateaabbeta.data = torch.tensor(150.0)
-
-    print("BEFORE SET")
-    print(pmodel.flow_rateaabbeta)
-    print(nmodel.named_parameters()["flow_rate.eta"].tensor().tensor())
-
-    y, J = pmodel(S, F)
-
-    print("AFTER")
-    print(pmodel.flow_rateaabbeta)
-    print(nmodel.named_parameters()["flow_rate.eta"].tensor().tensor())
-
-    print(pmodel.flow_rateaabbeta.requires_grad)
-    print(nmodel.named_parameters()["flow_rate.eta"].requires_grad)
-    print(y.requires_grad)
-    print(J.requires_grad)
-
-    test = torch.norm(y)
-    test.backward()
-
-    print(nmodel.named_parameters()["flow_rate.eta"].grad)
-    print(pmodel.flow_rateaabbeta.grad)
-
-    sys.exit()
-
     solver = nonlinear.RecursiveNonlinearEquationSolver(
         pmodel,
         initial_state,
         step_generator=nonlinear.StepGenerator(10),
         predictor=nonlinear.PreviousStepsPredictor(),
     )
-    res = solver.solve(ntime, forces)
-
-    print(pmodel.yieldaabbsy)
-    print(nmodel.named_parameters()["yield.sy"].tensor().tensor())
+    # Uncomment this line to use non-adjoint
+    # res = solver.solve(ntime, forces)
+    res = nonlinear.solve_adjoint(solver, ntime, forces)
 
     whatever = torch.norm(res)
-    print(whatever)
     whatever.backward()
 
-    print(pmodel.yieldaabbsy)
+    print(pmodel.yieldaabbsy.grad)
 
     plt.plot(strain[:, 0, 0], res[:, 0, 0].detach().numpy())
     plt.plot(strain[:, -1, 0], res[:, -1, 0].detach().numpy())
