c6a55d4f0ade338c3ebfd4a3ef89e0ec82cf7cd7
Mark Messner <mark.messner@gmail.com>
2024-09-11 16:55:11 -0500

    Need a random package to render the progress bars in the notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -36,3 +36,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+# PDF options
+latex_elements = {
+        'preamble': r'''
+\usepackage{pmboxdraw}
+        '''
+        }
